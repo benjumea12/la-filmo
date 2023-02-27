188685c70492a07e5ed92c1a001a813cccd5a69c
Esteban Benjumea <benjumeac12@gmail.com>
2023-02-27 10:55:49 -0500

    add header

--- wp-content/themes/cms-cortos/header.php
+++ wp-content/themes/cms-cortos/header.php
@@ -1,7 +1,12 @@
 <nav id="nav-page">
-  <h3>GMA</h3>
-  <div>
-    <a href="<?php echo get_site_url(); ?>/autores">Autores</a>
-    <a href="<?php echo get_site_url(); ?>/cortometrajes">Cortometrajes</a>
+  <div class="nav-contain">
+    <div class="nav-logo">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-light.svg" alt="Logo de GMA">
+      <h3>GMA</h3>
+    </div>
+    <div>
+      <a href="<?php echo get_site_url(); ?>/autores">Autores</a>
+      <a href="<?php echo get_site_url(); ?>/cortometrajes">Cortometrajes</a>
+    </div>
   </div>
 </nav>
